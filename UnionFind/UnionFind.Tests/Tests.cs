52a5d665c4be326c6517f762486ffa1e9e11c214
arboktya <howaboutno129@gmail.com>
2023-12-15 18:29:05 +0300

    добавил СНМ

---
+++ UnionFind/UnionFind.Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace UnionFind.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void BothOperations()
+        {
+            var cnm = new UnionFind(7);
+
+            Assert.Equal(1, cnm.Find(1));
+            Assert.Equal(5, cnm.Find(5));
+
+            cnm.Union(0, 1);
+            Assert.Equal(0, cnm.Find(1));
+
+            cnm.Union(0, 2);
+            Assert.Equal(0, cnm.Find(2));
+
+            cnm.Union(4, 5);
+            cnm.Union(5, 6);
+            cnm.Union(5, 0);
+
+            Assert.Equal(4, cnm.Find(0));
+        }
+    }
+}
---
+++ UnionFind/UnionFind/UnionFind.cs
@@ -0,0 +1,36 @@
+﻿namespace UnionFind
+{
+    public class UnionFind
+    {
+        private int[] ranks;
+        private int[] parents;
+
+        public UnionFind(int size)
+        {
+            ranks = new int[size];
+            parents = new int[size];
+            for(int i = 0; i < size; ++i)
+                parents[i] = i;
+        }
+
+        public int Find(int elem)
+        {
+            return parents[elem] = parents[elem] == elem
+                    ? elem
+                    : Find(parents[elem]);
+        }
+
+        public void Union(int a, int b)
+        {
+            var aRoot = Find(a);
+            var bRoot = Find(b);
+
+            if (ranks[aRoot] == ranks[bRoot])
+                ++ranks[aRoot];
+            if (ranks[aRoot] > ranks[bRoot])
+                parents[bRoot] = parents[aRoot];
+            else
+                parents[aRoot] = parents[bRoot];
+        }
+    }
+}
