63c1a07779ff1eb4633288d3a046af03f40fc8cd
arboktya <howaboutno129@gmail.com>
2023-12-15 17:27:01 +0300

    добавил дерево отрезков на сумму

---
+++ SegmentTree/SegmentTree.Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace SegmentTree.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void GetSum()
+        {
+            var numbers = new List<long> { 1, 9, -3, 4, 5, 6, -5 };
+            var tree = new SegmentTree(numbers);
+
+            Assert.Equal(numbers.GetRange(0, 1).Sum(), tree.GetSum(0, 1));
+            Assert.Equal(numbers.GetRange(0, 2).Sum(), tree.GetSum(0, 2));
+            Assert.Equal(numbers.GetRange(0, 3).Sum(), tree.GetSum(0, 3));
+            Assert.Equal(numbers.GetRange(2, 5).Sum(), tree.GetSum(2, 7));
+            Assert.Equal(numbers.GetRange(0, numbers.Count).Sum(), tree.GetSum(0, numbers.Count));
+            Assert.Equal(numbers.GetRange(3, 2).Sum(), tree.GetSum(3, 5));
+        }
+
+        [Fact]
+        public void ChangeElems()
+        {
+            var numbers = new List<long> { 1, 9, -3, 4, 5, 6, -5 };
+            var tree = new SegmentTree(numbers);
+
+            tree.Set(0, -5);
+            Assert.Equal(-5, tree.GetSum(0, 1));
+
+            tree.Set(numbers.Count / 2, 0);
+            Assert.Equal(0, tree.GetSum(numbers.Count / 2, numbers.Count / 2 + 1));
+
+            tree.Set(numbers.Count - 1, 0);
+            Assert.Equal(0, tree.GetSum(numbers.Count - 1, numbers.Count));
+        }
+    }
+}
---
+++ SegmentTree/SegmentTree/SegmentTree.cs
@@ -0,0 +1,71 @@
+﻿namespace SegmentTree
+{
+    public class SegmentTree
+    {
+        private long[] tree;
+        private int size;
+
+        public SegmentTree(List<long> numbers)
+        {
+            size = 1;
+            while (size < numbers.Count)
+                size *= 2;
+            tree = new long[size * 2 - 1];
+            Build(numbers, 0, 0, size);
+        }
+
+        private SegmentTree() { }
+
+        private void Build(List<long> numbers, int x, int lx, int rx)
+        {
+            if (rx - lx == 1)
+            {
+                if (lx < numbers.Count)
+                    tree[x] = numbers[lx];
+                return;
+            }
+            int m = (lx + rx) / 2;
+            Build(numbers, 2 * x + 1, lx, m);
+            Build(numbers, 2 * x + 2, m, rx);
+            tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+        }
+
+        public void Set(int index, long value)
+            => Set(index, value, 0, 0, size);
+
+        private void Set(int index, long value, int x, int lx, int rx)
+        {
+            if(rx - lx == 1)
+            {
+                tree[x] = value;
+                return;
+            }
+
+            int m = (lx + rx) / 2;
+
+            if (index < m)
+                Set(index, value, 2 * x + 1, lx, m);
+            else
+                Set(index, value, 2 * x + 2, m, rx);
+
+            tree[x] = tree[2 * x + 1] + tree[2 * x + 2];
+        }
+
+        public long GetSum(int l, int r)
+            => GetSum(l, r, 0, 0, size);
+
+        private long GetSum(int l, int r, int x, int lx, int rx)
+        {
+            if (lx >= r || rx <= l)
+                return 0;
+            if (l <= lx && rx <= r)
+                return tree[x];
+
+            int m = (lx + rx) / 2;
+            long leftSum = GetSum(l, r, 2 * x + 1, lx, m);
+            long rightSum = GetSum(l, r, 2 * x + 2, m, rx);
+
+            return leftSum + rightSum;
+        }
+    }
+}
